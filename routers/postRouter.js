1a08ad6772f41d855610c0468353c262e254bcf2
biancapcr <biancacristina.pocora@gmail.com>
2025-08-05 10:39:11 +0200

    [upd] (add) destructuring functions in postrouter.js

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -2,7 +2,35 @@
 const express = require('express');
 const router = express.Router();
 
+// destrutturo le funzioni dal controller
+const {
+  index,
+  show,
+  create,
+  modify, 
+  update,
+  destroy
+} = require('../controllers/postsController');
 
+// index
+router.get('/', index);
+
+// show
+router.get('/:id', show);
+
+// create
+router.post('/', create);
+
+// modify
+router.put('/:id', modify);
+
+// update
+router.patch('/:id', update);
+
+// destroy
+router.delete('/:id', destroy);
+
+module.exports = router;
 
 
 
